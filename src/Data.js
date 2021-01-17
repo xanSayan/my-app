edbc71f37f753287d74f1751606305d0c0e9c2dc
xanSayan <sayan.tffs@gmail.com>
2021-01-17 13:37:25 +0530

    added dummy data

--- src/Data.js
+++ src/Data.js
@@ -2,29 +2,41 @@ export const Data = {
   Users: [
     {
       name: "James Bond",
-      email: "james@bondmail.com"
+      email: "james@bondmail.com",
     },
     {
       name: "David Letterman",
-      email: "david@lettermail.com"
+      email: "david@lettermail.com",
     },
     {
       name: "Bruce Lee",
-      email: "bruce@leemail.com"
-    }
+      email: "bruce@leemail.com",
+    },
   ],
   ToDos: [
     {
       todoAction: "Action No 1",
-      dateAdded: "2015-01-25"
+      dateAdded: "2015-01-25",
     },
     {
       todoAction: "Action No 2",
-      dateAdded: "2015-03-05"
+      dateAdded: "2015-03-05",
     },
     {
       todoAction: "Action No 3",
-      dateAdded: "2015-07-21"
-    }
-  ]
+      dateAdded: "2015-07-21",
+    },
+    {
+      todoAction: "Action No 4",
+      dateAdded: "2015-02-25",
+    },
+    {
+      todoAction: "Action No 5",
+      dateAdded: "2015-05-05",
+    },
+    {
+      todoAction: "Action No 6",
+      dateAdded: "2015-09-21",
+    },
+  ],
 };
